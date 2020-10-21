ef0023cb8390f8b073c3c2860af2550281975b18
Skalice132 <58006458+Skalice132@users.noreply.github.com>
2020-10-22 00:40:58 +0400

    init: start task

--- src/index.js
+++ src/index.js
@@ -36,11 +36,42 @@ const MORSE_TABLE = {
     '----.':  '9',
     '-----':  '0',
 };
-
+// каждый символ это десять нулей. если символ равен точке, то это 10, если тире, то 11
 function decode(expr) {
-    // write your solution here
+  // console.log(expr.length)
+  // str.repeat()
+  let str = "0";
+  let result = ''; 
+
+
+  for (var i = expr.length - 1; i >= 0; i--) {
+    for (key in MORSE_TABLE) {
+      if (expr[i] == MORSE_TABLE[key]){
+        result += key;
+      }
+      // console.log(key); // символы
+      // console.log(MORSE_TABLE[key]); //  буквы
+    }
+  // console.log(expr[i]);
+  // console.log(MORSE_TABLE.expr[i]);
+  // result += expr[i];
+  }
+  console.log(result)
+  if (result.length < 10) {
+    result = str.repeat(10 - result.length) + result;
+    return result;
+  } else {
+    return result;
+  }
 }
 
 module.exports = {
     decode
 }
+
+console.log(decode('me'));
+// decode('me');
+// console.log(decode('hello world'));
+// decode('hello world');
+
+decode('829b4b8c');
